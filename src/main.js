9840d999dbac3e83a27fc3a62fdb17b6ef68771d
Abdullah Al Noman <abdullahalnoman1512@gmail.com>
2021-07-20 22:47:37 +0600

    edd

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import * as Vue from 'vue';
+import * as VueRouter from 'vue-router';
+
+import App from './App.vue';
+import routes from './routes';
+import store from './store';
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+});
+
+Vue.createApp(App).use(store)
+  .use(router)
+  .mount('#app');
